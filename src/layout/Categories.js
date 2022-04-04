f23c42094d24e43c6690216716f7a6677e1d7643
Faruk-Karaman <faruk_karaman1@hotmail.com>
2022-04-05 00:26:58 +0300

    05-04-2022

--- src/layout/Categories.js
+++ src/layout/Categories.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { Menu } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Menu, Table } from "semantic-ui-react";
+import ProductService from "../services/productService";
 export default function Categories() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    let productService = new ProductService();
+    productService
+      .getCategories()
+      .then((result) => setCategories(result.data.data));
+  }, []);
   return (
     <div>
       <Menu pointing vertical>
-        <Menu.Item name="home" />
-        <Menu.Item name="messages" />
+        <Menu.Item name="kategoriler" />
+        {categories.map((category) => (
+          <Menu.Item key={category.categoryId} name={category.categoryName} />
+        ))}
+
         <Menu.Item name="friends" />
       </Menu>
     </div>
